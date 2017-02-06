26966ed248f5acd02baded74015c6179bb7794ae
Tom Muijsers <tom.muijsers+git@gmail.com>
2017-02-06 21:57:29 +0100

    Added R files to produce the plots, along with the plots in png

---
+++ plot1.R
@@ -0,0 +1,62 @@
+# Set locale for English Date names
+
+Sys.setlocale("LC_TIME", "C")
+
+
+# Download and unzip data if necessary
+
+if (!file.exists("household_power_consumption.txt")) {
+    
+    if (!file.exists("household_power_consumption.zip")){
+        
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(url = fileUrl, destfile = "household_power_consumption.zip")
+        
+    }
+    
+    unzip(zipfile = "household_power_consumption.zip")
+    
+}
+
+
+# Read the date into a data frame
+
+dt <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";")
+
+
+# Take only the relevant data
+
+dt <- subset(dt, as.Date(Date, format = "%d/%m/%Y") >= as.Date("2007-02-01") &
+                 as.Date(Date, format = "%d/%m/%Y") <= as.Date("2007-02-02"))
+
+
+# Create DateTime column by merging the Date and Time columns,
+# and then converting to "POSIXlt"
+
+dt$DateTime <- with(dt, paste(Date, Time, sep = " "))
+dt$DateTime <- strptime(dt$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+
+# Set "?" values to NA
+
+to_NA <- function(x){
+    x[x == "?"] <- NA
+    x
+}
+
+dt[,3:8] <- lapply(dt[,3:8], to_NA)
+
+
+# Convert factor to numeric
+
+dt[,3:8] <- lapply(dt[,3:8], function(x) as.numeric(levels(x))[x])
+
+
+# Plot
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+hist(dt$Global_active_power, breaks = 12, col = "red",
+     main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,67 @@
+# Set locale for English Date names
+
+Sys.setlocale("LC_TIME", "C")
+
+
+# Download and unzip data if necessary
+
+if (!file.exists("household_power_consumption.txt")) {
+    
+    if (!file.exists("household_power_consumption.zip")){
+        
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(url = fileUrl, destfile = "household_power_consumption.zip")
+        
+    }
+    
+    unzip(zipfile = "household_power_consumption.zip")
+    
+}
+
+
+# Read the date into a data frame
+
+dt <- read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";")
+
+
+# Take only the relevant data
+
+dt <- subset(dt, as.Date(Date, format = "%d/%m/%Y") >= as.Date("2007-02-01") &
+                 as.Date(Date, format = "%d/%m/%Y") <= as.Date("2007-02-02"))
+
+
+# Create DateTime column by merging the Date and Time columns,
+# and then converting to "POSIXlt"
+
+dt$DateTime <- with(dt, paste(Date, Time, sep = " "))
+dt$DateTime <- strptime(dt$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+
+# Set "?" values to NA
+
+to_NA <- function(x){
+    x[x == "?"] <- NA
+    x
+}
+
+dt[,3:8] <- lapply(dt[,3:8], to_NA)
+
+
+# Convert factor to numeric
+
+dt[,3:8] <- lapply(dt[,3:8], function(x) as.numeric(levels(x))[x])
+
+
+# Plot
+
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+
+with(dt, plot(DateTime, Sub_metering_1, type = "l",
+              xlab = NA, ylab = "Energy sub metering"))
+with(dt, lines(DateTime, Sub_metering_2, type = "l", col = "red"))
+with(dt, lines(DateTime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lwd=1, col=c("black", "red", "blue"), lty=c(1,1,1))
+
+dev.off()
